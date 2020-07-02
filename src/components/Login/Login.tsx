fc02067aaa40c1418e35c541be6fa7fbe3075b5b
Ever Uriel Garcia <uriel621@live.com>
2020-07-01 23:49:32 -0600

    Notes and comments on what to do next

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import {Link} from 'react-router-dom';
 import {Button, Form, Grid, Header, Message, Segment} from 'semantic-ui-react'
 
+// TODO Navigation bar
+// TODO FORMIK NEXT FOR LOGIN/REGISTERING
+
 const Login = () => (
   <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
     <Grid.Column style={{ maxWidth: 450 }}>
